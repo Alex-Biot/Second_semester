048b2054b1a679b075697c568942cece9ea6ed4c
Alex-Biot <biot@yandex.ru>
2020-08-21 10:06:12 +0200

    Tarakan mistake

--- Сockroach/src/Main.java
+++ Сockroach/src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 // Then join all the tarakans. Then print the scores out.
         List<Score> totalScore = new ArrayList<>();
         Cockroach[] cockroaches = new Cockroach[10];
-        Thread cockroachesThread;
+        Thread [] cockroachesThread=new Thread[10];
         int stepNum = 20;
         for (int i = 0; i < 10; i++) {
             cockroaches[i] = new Cockroach(stepNum, "Cockroach" + i);
-            cockroachesThread = new Thread(cockroaches[i]);
-            cockroachesThread.start();
-            cockroachesThread.join();
+            cockroachesThread[i] = new Thread(cockroaches[i]);
+            cockroachesThread[i].start();
+            cockroachesThread[i].join(i);
         }
         for (int i = 0; i < 10; i++) {
             totalScore.add(cockroaches[i].scores.get(0));
